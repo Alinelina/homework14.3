bc3e94fe19804fd4829ba2ad21efb1edcd9088a5
Alina <91183241+Alinelina@users.noreply.github.com>
2022-01-21 16:13:06 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+
+function changeImageLeft() {
+    let ImgPlantGreen = document.getElementById("image-plant");
+    console.log(ImgPlantGreen);
+    ImgPlantGreen.src = "/assets/images/plants-green.png";
+}
+
+function changeImageRight() {
+    let ImgPlantYellow = document.getElementById("image-plant");
+    console.log(ImgPlantYellow);
+    ImgPlantYellow.src = "/assets/images/plants-yellow.png";
+}
